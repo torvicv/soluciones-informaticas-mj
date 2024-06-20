0fad295627ac08ad8a65da84c382c4a4d6de26ed
victor <torvicv@gmail.com>
2024-06-21 00:25:16 +0200

    creada migracion, controlador, modelo y request

---
+++ app/Http/Controllers/DiasFestivosController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\DiasFestivos;
+use Illuminate\Http\Request;
+
+class DiasFestivosController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(DiasFestivos $diasFestivos)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(DiasFestivos $diasFestivos)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, DiasFestivos $diasFestivos)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(DiasFestivos $diasFestivos)
+    {
+        //
+    }
+}
